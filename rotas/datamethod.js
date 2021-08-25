aa39ea8522c53670eef6ff739630a2566b5e33ba
Giovanni <giovanni.marcos.leite@gmail.com>
2021-08-25 15:33:09 -0300

    enviar arquivos funcionando, enviando imagens para o S3 em categoria diferente.

--- rotas/datamethod.js
+++ rotas/datamethod.js
@@ -8,14 +8,7 @@ module.exports = {
 
     b64toimg(dadosrecebidos, id, req, res) {
         var buf = Buffer.from(`${dadosrecebidos["imagem"]}`, 'base64')
-        fs.writeFileSync(`./saved-data/${id}.png`, buf)
-    },
-    find(id, req, res) {
-        var cpf = fs.readFileSync(`saved-data/${id}.json`)
-        let dados = JSON.parse(cpf)
-        res.status(201)
-        res.send(JSON.stringify(dados["cpf"]))
-
+        fs.writeFileSync(`./saved-data/${id}.jpeg`, buf)
     },
     pegardatastring() {
         const date = new Date()
@@ -36,15 +29,15 @@ module.exports = {
         })
 
         const s3 = new AWS.S3()
-        const filecontent = fs.readFileSync(`./saved-data/${id}.png`)
+        const filecontent = fs.readFileSync(`./saved-data/${id}.jpeg`)
         let cpf = cpfrecebido
 
         var params = {
             Body: filecontent,
             ContentEncoding: 'base64',
-            ContentType: 'image/png',
+            ContentType: 'image/jpeg',
             Bucket: "skill-hive-face",
-            Key: `source/${id}.png`,
+            Key: `source/${id}.jpeg`,
             Metadata: {
                 "cpf": `${cpf}`
             }
@@ -54,7 +47,42 @@ module.exports = {
             else console.log(data)
 
         })
+    },
+    uploadtextbreaks3(id) {
+        const { AWS_ACESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env
+
+        AWS.config.update({
+            accessKeyId: 'AKIA6LVBLYD6GA5QRK3P',
+            secretAccessKey: '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',
+            region: 'us-east-1'
+        })
+
+        const s3 = new AWS.S3()
+        const filecontent = fs.readFileSync(`./saved-data/${id}.jpeg`)
+
+        var params = {
+            Body: filecontent,
+            ContentEncoding: 'base64',
+            ContentType: 'image/jpeg',
+            Bucket: "skill-hive-face",
+            Key: `source/${id}.jpeg`,
+        }
+        s3.putObject(params, function (err, data) {
+            if (err) console.log(err, err.stack)
+            else console.log(data)
+
+        })
+    },
+    savearq(id, req, res) {
+        var tmp_path = req.file.path
+        var target_path = 'saved-data/' + req.file.originalname
+        var src = fs.createReadStream(tmp_path)
+        var dest = fs.createWriteStream(target_path)
+        src.pipe(dest)
+        src.on('end', () => {res.send('Complete')})
+        src.on('error', (err) => {res.json({ error: err })})
     }
+
 }
 
 
--- rotas/index.js
+++ rotas/index.js
@@ -1,8 +1,9 @@
 const roteador = require('express').Router()
 const dados = require('./datamethod')
 const fs = require('fs')
-const { inserir, b64toimg, clear, find, atualizarid, pegarid, uploads3, pegardatastring } = require('./datamethod')
-
+const { inserir, b64toimg, clear, find, atualizarid, pegarid, uploads3, pegardatastring, uploadtextbreaks3, savearq } = require('./datamethod')
+const multer = require('multer')
+const upload = multer({ dest: 'saved-data' })
 
 
 roteador.get('/facialrec', async (req, res) => {
@@ -10,14 +11,40 @@ roteador.get('/facialrec', async (req, res) => {
 })
 
 roteador.post('/facialrec', async (req, res) => {
-    var id = pegardatastring()
-    const dadosrecebidos = req.body
-    const cpfrecebido = dadosrecebidos["cpf"]
-    b64toimg(dadosrecebidos, id, req, res)
-    uploads3(id, cpfrecebido)
-    res.status(201)
+    try {
+        var id = pegardatastring()
+        const dadosrecebidos = req.body
+        const cpfrecebido = dadosrecebidos["cpf"]
+        b64toimg(dadosrecebidos, id, req, res)
+        uploads3(id, cpfrecebido)
+        res.status(201)
+    } catch (erro) {
+        res.send(JSON.stringify(erro))
+        res.status(400)
+    }
+
 })
 
+roteador.post('/text', async (req, res) => {
+    try {
+        var id = pegardatastring()
+        const dadosrecebidos = req.body
+        b64toimg(dadosrecebidos, id, req, res)
+        uploadtextbreaks3(id)
+        res.send('Enviado com sucesso.')
+        res.status(201)
+    } catch (erro) {
+        res.send(JSON.stringify(erro))
+        res.status(400)
+    }
+})
+
+roteador.post('/img', upload.single('formImage'), async (req, res) => {
+    console.log(req.file)
+    var id = pegardatastring()
+    await savearq(id, req, res)
+    res.status(201)
+})
 
 module.exports = roteador
 
