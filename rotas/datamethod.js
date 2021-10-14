ea77c8487897c159383f14f705758801697dd65a
Giovanni <giovanni.marcos.leite@gmail.com>
2021-10-14 19:06:32 -0300

    main update, serverless

--- rotas/datamethod.js
+++ rotas/datamethod.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 const AWS = require('aws-sdk')
 const { fromPath } = require('pdf2pic')
 AWS.config.update({
-    accessKeyId: 'ACESS KEY ID HERE',
-    secretAccessKey: 'SECRET ACESS KEY HERE',
-    region: 'REGION HERE'
+    accessKeyId: 'AKIA6LVBLYD6GA5QRK3P',
+    secretAccessKey: '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',
+    region: 'us-east-1'
 })
 const { AWS_ACESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env
 
@@ -25,70 +25,16 @@ module.exports = {
         return `${year}${month}${day}${seconds}`
 
     },
-    uploads3(id, cpfrecebido) {
-        const s3 = new AWS.S3()
-        const filecontent = fs.readFileSync(`./saved-data/${id}.jpeg`)
-        let cpf = cpfrecebido
 
-        var params = {
-            Body: filecontent,
-            ContentEncoding: 'base64',
-            ContentType: 'image/jpeg',
-            Bucket: "ip2-api-dev",
-            Key: `source/${id}.jpeg`,
-            Metadata: {
-                "cpf": `${cpf}`
-            }
-        }
-        s3.putObject(params, function (err, data) {
-            if (err) console.log(err, err.stack)
-            else console.log(data)
+    presignedurl(id) {
+        var s3 = new AWS.S3();
 
+        var presignedGETURL = s3.getSignedUrl('postObject', {
+            Bucket: 'ip2-api-dev',
+            Key: `${id}.jpeg`, //filename
+            Expires: 100 //time to expire in seconds
         })
-    },
-    uploadimgs3(id) {
-        const s3 = new AWS.S3()
-        const filecontent = fs.readFileSync(`./saved-data/${id}.1.jpeg`)
-        var params = {
-            Body: filecontent,
-            Bucket: "ip2-api-dev",
-            Key: `${id}.jpeg`,
-        }
-        s3.putObject(params, function (err, data) {
-            if (err) console.log(err, err.stack)
-            else console.log(data)
-
-        })
-
-    },
-    pdftoimg(id) {
-        const options = {
-            density: 300,
-            saveFilename: `${id}`,
-            savePath: "./saved-data/",
-            format: "jpeg",
-            width: 2480,
-            height: 3508
-        };
-        const storeAsImage = fromPath(`./saved-data/${id}.pdf`, options);
-        const pageToConvertAsImage = 1;
-
-        storeAsImage(pageToConvertAsImage).then((resolve) => {
-            console.log("Convertido para imagem.");
-
-            return resolve;
-        });
-
-    },
-    limpararquivos(id) {
-        fs.unlink(`./saved-data/${id}.pdf`, (err) => {
-            if (err) console.log(err)
-            else console.log('PDF Deletado')
-        })
-        fs.unlink(`./saved-data/${id}.1.jpeg`, (err) => {
-            if (err) console.log(err)
-            else console.log('JPEG Deletado')
-        })
+        return presignedGETURL;
     }
 }
 
